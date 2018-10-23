58ef7ed67ca234e2f4097e55a362248270a38267
maximiliano-rdl <maximiliano.rdl@gmail.com>
2018-10-23 17:52:23 -0300

    Agregada comunicacion basica entre cliente y servidor mediant shm

--- Ejercicio_5/src/servidor/servidor.c
+++ Ejercicio_5/src/servidor/servidor.c
@@ -3,6 +3,11 @@
 #include <sys/shm.h>
 #include <protocol.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
 
 void err_sys(char* err_msg){
 	fprintf(stderr, "%s\n", err_msg);
@@ -11,8 +16,27 @@ void err_sys(char* err_msg){
 int main(){
 	char* home = getenv("HOME");
 	s_config configuracion;
+	int fd;
 	load_config(strcat(home,"/.config/adlms/Ejercicio5/conf.conf"),&configuracion);
-	printf("Estas son las configuraciones:\nHOME:%s\nNombre Mem: %s\nTamanio Mem: %d",home,configuracion.name,configuracion.size);
+	printf("Estas son las configuraciones:\nHOME:%s\nNombre Mem: %s\nTamanio Mem: %d\n",home,configuracion.name,configuracion.size);
+	
+	fd = shm_open(configuracion.name, O_CREAT | O_EXCL | O_RDWR, 0600);
+	if(fd<0){
+		perror("shm_open()");
+		return EXIT_FAILURE;
+	}
 
-	exit(0);
+	ftruncate(fd, configuracion.size);
+
+	int *data = (int *)mmap(0, configuracion.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	printf("sender mapped address: %p\n",data);
+
+	for(int i = 0; i < 4; ++i){
+		data[i]=i;
+	}
+	munmap(data, configuracion.size);
+
+	close(fd);
+
+	return EXIT_SUCCESS;
 }
