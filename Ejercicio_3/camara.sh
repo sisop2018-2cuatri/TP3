ace4b0851d24f7b1bddb97e0af59a1a583700762
lautaro <lautaro.lorenz@gmail.com>
2018-11-21 23:46:58 -0300

    arreglos de testing

--- Ejercicio_3/camara.sh
+++ Ejercicio_3/camara.sh
@@ -4,6 +4,7 @@
 # son para poder hacer testing.
 
 FIFO_CAMARAS="/tmp/camaras"
+
 echo "EAAA999 09:12" >> $FIFO_CAMARAS
 sleep 1
 echo "EIII111 09:00" >> $FIFO_CAMARAS
@@ -20,19 +21,15 @@ echo "SBBB000 20:00" >> $FIFO_CAMARAS
 sleep 1
 echo "SAAA999 20:30" >> $FIFO_CAMARAS
 sleep 1
-echo "EBB000ZZ 21:05" >> $FIFO_CAMARAS
+echo "EBBB000 21:05" >> $FIFO_CAMARAS
 sleep 1
 echo "SIII111 22:00" >> $FIFO_CAMARAS
 sleep 1
-echo "EAA222BB 16:24" >> $FIFO_CAMARAS
-sleep 1
-echo "EAA333CC 12:45" >> $FIFO_CAMARAS
-sleep 1
-echo "SAA333CC 22:04" >> $FIFO_CAMARAS
-sleep 1
-echo "SAA222BB 23:37" >> $FIFO_CAMARAS
-sleep 1
-echo "EASD123 15:13" >> $FIFO_CAMARAS
-sleep 1
-echo "SASD123 17:28" >> $FIFO_CAMARAS
-sleep 1
+
+
+
+
+
+
+
+
